fa1a9e3a5bd95cf77d00058b976cf734fb750af5
Chai Phonbopit <phonbopit@gmail.com>
2020-08-22 16:41:29 +0700

    64cb1b2

---
+++ _nuxt/static/1598089030/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1598089030",layout:"default",error:a,state:{myState:"this is state",counter:0,name:"Devahoy",photos:{list:[],isFetching:b},users:{list:[],detail:a,loggedIn:b},auth:{user:a,loggedIn:b,strategy:"local"}},serverRendered:true,routePath:"\u002F",config:{}}}(null,false));
